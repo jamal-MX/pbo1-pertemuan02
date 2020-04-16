33e9c6d8f52b5809bc9f8d7c86a6ba57cc60bbbc
JAMAL-MX <pointleopard007@gmail.com>
2020-04-16 20:25:18 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+	public static void tampilaninfo(){
+	System.out.println();
+	System.out.println("==Data Mahasiswa=============");
+	System.out.println("NPM : 18630319");
+	System.out.println("Nama : Jamalludin ");
+	System.out.println("=============================");
+	}
+}
